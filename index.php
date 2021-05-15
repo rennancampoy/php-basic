7cac68a645048341671cc463a05968d0fab9f21a
rennancampoy <rennancampoy@yahoo.com.br>
2021-05-15 04:27:09 -0300

    getting props values in a array of objects

--- index.php
+++ index.php
@@ -11,8 +11,20 @@ class Person
 		$this->name = $name;
 		$this->age = $age;
 	}
+
+	function getProp(string $propName)
+	{
+		return $this->$propName;
+	}
 }
 
 $person = new Person("Rennan", 26);
 
+$arrayPersons = array(new Person("A", 1), new Person("B", 2), new Person("C", 3), new Person("D", 4));
+
 var_dump($person);
+var_dump($arrayPersons);
+
+foreach ($arrayPersons as $person) {
+	echo $person->getProp("age") . "<br/>;
+}
