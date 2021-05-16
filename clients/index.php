ec5282d0302248cfb043f0e89a937448f2eb77c1
rennancampoy <rennancampoy@yahoo.com.br>
2021-05-15 21:57:01 -0300

    bootstrap ini + composer

---
+++ clients/index.php
@@ -0,0 +1,22 @@
+<?php
+
+include __DIR__.'/includes/header.php';
+include __DIR__.'/includes/footer.php';
+
+class Person
+{
+
+	private $name;
+	private $age;
+
+	function __construct(string $name, int $age)
+	{
+		$this->name = $name;
+		$this->age = $age;
+	}
+
+	function getProp(string $propName)
+	{
+		return $this->$propName;
+	}
+}
