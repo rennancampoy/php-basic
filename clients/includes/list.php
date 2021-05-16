12e7c96fc2e2f6ecb448e939ef629c01d47c2343
rennancampoy <rennancampoy@yahoo.com.br>
2021-05-16 03:44:16 -0300

    button for edit and delete client

--- clients/includes/list.php
+++ clients/includes/list.php
@@ -15,6 +15,7 @@
 					<th>Name</th>
 					<th>Age</th>
 					<th>Email</th>
+					<th>Actions</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -24,6 +25,19 @@
 					<td><?= $client->getProp('name'); ?></td>
 					<td><?= $client->getProp('age'); ?></td>
 					<td><?= $client->getProp('email'); ?></td>
+					<td>
+						<a href="edit.php?id=<?php echo $client->getProp('id'); ?>">
+							<button type="button" class="btn btn-primary">
+								Edit
+							</button>
+						</a>
+
+						<a href="delete.php?id=<?php echo $client->getProp('id'); ?>">
+							<button type="button" class="btn btn-danger">
+								Delete
+							</button>
+						</a>
+					</td>
 				</tr>
 				<?php endforeach; ?>
 			</tbody>
